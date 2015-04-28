a275e954d5204ef4dcc4ff339348c6e92393897a
lennoxkao <as820416@gmail.com>
2015-04-28 19:09:36 +0800

    test again

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,8 +17,7 @@ void MainWindow::on_load_clicked()
 {
     cv::Mat test = cv::imread("123.jpg");
     cv::imshow("test",test);
-    qDebug() << test.at<cv::Vec3b>(1500,2000)[0] << test.at<cv::Vec3b>(1500,2000)[1]<< test.at<cv::Vec3b>(1500,2000)[2];
-    qDebug() << test.rows << test.cols;
+
     cv::resize(test,test,cv::Size(400,300));
     QImage img((const unsigned char*)(test.data), test.cols, test.rows, QImage::Format_RGB888);
     ui->label->setPixmap(QPixmap::fromImage(img));
